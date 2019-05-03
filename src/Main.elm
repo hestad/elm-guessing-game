4a600ded0f3b1cdc01069d291c577055ae898f8e
John Andre Hestad <john.hestad@blank.no>
2019-05-03 15:04:42 +0200

    Show error when you already guessed an item.

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,16 @@
 -- module Main exposing (Model, Msg(..), hint, init, kommuneSet, kommuner, main, renderList, subscriptions, update, view)
 
+
+module Main exposing (Kommune, Model, Msg(..), createHint, createResponse, getKommuner, init, kommuneDecoder, kommunerDecoder, main, renderList, subscriptions, tryGetKommune, update, view)
+
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Set
 import Http
-import Json.Decode exposing (Decoder, field, int, string, list, map2)
+import Json.Decode exposing (Decoder, field, int, list, map2, string)
+import Set
+
 
 
 -- MAIN
@@ -26,14 +30,15 @@ main =
 
 
 type alias Kommune =
-    { name: String,
-      code: String
+    { name : String
+    , code : String
     }
+
 
 type alias Model =
     { guessInputContent : String
     , correct : List Kommune
-    , kommuner: List Kommune
+    , kommuner : List Kommune
     }
 
 
@@ -49,8 +54,8 @@ init _ =
 
 
 type Msg
-    = Guess String |
-      GotKommuner (Result Http.Error (List Kommune))
+    = Guess String
+    | GotKommuner (Result Http.Error (List Kommune))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -60,15 +65,21 @@ update msg model =
             let
                 ( newGuess, newCorrect ) =
                     case tryGetKommune model.kommuner inputGuess of
-                            Just kommune -> ( "", kommune :: model.correct )
-                            Nothing -> ( inputGuess, model.correct )
+                        Just kommune ->
+                            ( "", kommune :: model.correct )
+
+                        Nothing ->
+                            ( inputGuess, model.correct )
             in
             ( { model | guessInputContent = newGuess, correct = newCorrect }, Cmd.none )
+
         GotKommuner response ->
             case response of
-                Err error -> Debug.todo (Debug.toString error)
-                Ok newKommuner ->  ( { model | kommuner=newKommuner}, Cmd.none )
+                Err error ->
+                    Debug.todo (Debug.toString error)
 
+                Ok newKommuner ->
+                    ( { model | kommuner = newKommuner }, Cmd.none )
 
 
 
@@ -90,11 +101,10 @@ view model =
         [ div [] [ Html.text ("Korrekt: " ++ String.fromInt (List.length model.correct) ++ " / " ++ String.fromInt (List.length model.kommuner)) ]
         , span []
             [ input [ value model.guessInputContent, type_ "text", placeholder "Tast inn en kommune", onInput Guess ] []
-            , span [] [ Html.text (hint model.kommuner model.guessInputContent) ]
+            , span [] [ Html.text (createResponse model.kommuner model.correct model.guessInputContent) ]
             ]
         , div [] [ renderList model.correct ]
         ]
-
 
 
 renderList lst =
@@ -103,40 +113,69 @@ renderList lst =
         |> Html.ul []
 
 
-tryGetKommune : (List Kommune) -> String -> Maybe Kommune
+tryGetKommune : List Kommune -> String -> Maybe Kommune
 tryGetKommune kommuner input =
     kommuner
         |> List.filter
-            (\n -> String.contains (String.toLower n.name) (String.toLower input))
+            (\n -> String.startsWith (String.toLower n.name) (String.toLower input))
         |> List.head
 
 
--- TODO: What if input matches multiple communes
+
+-- We just give hint on the first matched string in the list
 
 
-hint : (List Kommune) -> String -> String
-hint kommuner input =
+createResponse : List Kommune -> List Kommune -> String -> String
+createResponse kommuner correct input =
     case input of
         "" ->
             ""
 
         _ ->
-            kommuner
-                |> List.map
-                    (\n -> n.name)
-                |> List.filter
-                    (\n -> String.startsWith (String.toLower input) (String.toLower n))
-                |> List.head
-                |> Maybe.map
-                    String.length
-                |> Maybe.map
-                    (\n -> n - String.length input)
-                |> Maybe.map
-                    (\n -> String.repeat n "?")
-                |> Maybe.map
-                    (\n -> input ++ n)
-                |> Maybe.withDefault
-                    "Ukjent"
+            let
+                i =
+                    String.toLower input
+
+                k =
+                    List.map (\x -> String.toLower x.name) kommuner
+
+                c =
+                    List.map (\x -> String.toLower x.name) correct
+
+                partialMatch =
+                    k
+                        |> List.filter
+                            (\n -> String.startsWith i n)
+                        |> List.head
+            in
+            case partialMatch of
+                Nothing ->
+                    "Unknown"
+
+                Just s ->
+                    if s == i then
+                        s
+
+                    else if List.member s c then
+                        "You already guessed this!"
+
+                    else
+                        createHint s input
+
+
+createHint : String -> String -> String
+createHint partialMatch input =
+    String.length partialMatch
+        |> (\n ->
+                n
+                    - String.length input
+                    |> (\m ->
+                            String.repeat m "?"
+                                |> (\o -> input ++ o)
+                       )
+           )
+
+
 
 -- HTTP
 
@@ -144,17 +183,17 @@ hint kommuner input =
 getKommuner : Cmd Msg
 getKommuner =
     Http.get
-    {url = "kommuner.json"
-    , expect = Http.expectJson GotKommuner kommunerDecoder
-    }
-
+        { url = "kommuner.json"
+        , expect = Http.expectJson GotKommuner kommunerDecoder
+        }
 
 
 kommuneDecoder : Decoder Kommune
 kommuneDecoder =
-  map2 Kommune
-      (field "targetName" string)
-      (field "targetCode" string)
+    map2 Kommune
+        (field "targetName" string)
+        (field "targetCode" string)
+
 
 kommunerDecoder : Decoder (List Kommune)
 kommunerDecoder =
